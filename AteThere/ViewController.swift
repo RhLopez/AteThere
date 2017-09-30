a1c11c9773d6416353c807fa05a5eca2ca696aa5
RhLopez <ramiro.h.lopez@outlook.com>
2017-09-29 22:57:48 -0700

    Initial Commit

---
+++ AteThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AteThere
+//
+//  Created by Ramiro H Lopez on 9/29/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
