156da234cf11fee0cbb9be3380b2240446acc828
RhLopez <ramiro.h.lopez@outlook.com>
2017-10-11 00:57:26 -0700

    Add APIKey property

--- AteThere/Networking/FoursquareAPIClient.swift
+++ AteThere/Networking/FoursquareAPIClient.swift
@@ -9,18 +9,20 @@
 import Foundation
 
 class FoursquareAPIClient: APIClient {
+    var apiKey: APIKey
     var session: URLSession
     
-    init(sessionConfiguration: URLSessionConfiguration) {
+    init(sessionConfiguration: URLSessionConfiguration, apiKey: APIKey) {
         self.session = URLSession(configuration: sessionConfiguration)
+        self.apiKey = apiKey
     }
     
-    convenience init() {
-        self.init(sessionConfiguration: .default)
+    convenience init(apiKey: APIKey) {
+        self.init(sessionConfiguration: .default, apiKey: apiKey)
     }
     
     func search(withTerm term: String, completion: @escaping (Result<[Venue], APIError>) -> Void) {
-        let endpoint = Foursquare.search(term: term)
+        let endpoint = Foursquare.search(term: term, key: apiKey)
         
         let task = jsonTask(with: endpoint.request) { (json, error) in
             
@@ -44,7 +46,7 @@ class FoursquareAPIClient: APIClient {
     }
     
     func lookUp(forId id: String, completion: @escaping (Result<Venue, APIError>) -> Void) {
-        let endpoint = Foursquare.lookUp(id: id)
+        let endpoint = Foursquare.lookUp(id: id, key: apiKey)
         print(endpoint.request)
         let task = jsonTask(with: endpoint.request) { (json, error) in
             
@@ -71,3 +73,62 @@ class FoursquareAPIClient: APIClient {
         task.resume()
     }
 }
+
+enum Foursquare {
+    case search(term: String, key: APIKey)
+    case lookUp(id: String, key: APIKey)
+}
+
+extension Foursquare: Endpoint {
+    var base: String {
+        return Constants.base
+    }
+    
+    var path: String {
+        switch self {
+        case .search: return Constants.version + Constants.venues + Constants.search
+        case .lookUp(let id, _): return Constants.version + Constants.venues + "/\(id)"
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .search(let term, let key):
+            return [
+                URLQueryItem(name: Constants.client, value: key.clientID),
+                URLQueryItem(name: Constants.secret, value: key.clientSecret),
+                URLQueryItem(name: Constants.location, value: "33.881682,-118.117012"), // Remove hardcoded location
+                URLQueryItem(name: Constants.query, value: term),
+                URLQueryItem(name: Constants.versionParameter, value: Constants.versionDate),
+                URLQueryItem(name: Constants.mode, value: Constants.modeType)
+            ]
+        case .lookUp(_, let key):
+            return [
+                URLQueryItem(name: Constants.client, value: key.clientID),
+                URLQueryItem(name: Constants.secret, value: key.clientSecret),
+                URLQueryItem(name: Constants.versionParameter, value: Constants.versionDate),
+                URLQueryItem(name: Constants.mode, value: Constants.modeType)
+            ]
+        }
+    }
+}
+
+extension Foursquare {
+    struct Constants {
+        static let base = "https://api.foursquare.com"
+        static let version = "/v2"
+        static let venues = "/venues"
+        static let search = "/search"
+        static let query = "query"
+        static let client = "client_id"
+        static let secret = "client_secret"
+        static let intent = "intent"
+        static let radius = "radius"
+        static let location = "ll"
+        static let versionParameter = "v"
+        static let versionDate = "20170921"
+        static let mode = "mode"
+        static let modeType = "foursquare"
+    }
+}
+
