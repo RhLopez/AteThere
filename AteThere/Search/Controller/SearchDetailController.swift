2f8c9449d400d506f7a654b3edc098ec29d28091
RhLopez <ramiro.h.lopez@outlook.com>
2017-10-14 23:51:18 -0700

    add methods to get venue photos, custom back button

--- AteThere/Search/Controller/SearchDetailController.swift
+++ AteThere/Search/Controller/SearchDetailController.swift
@@ -17,9 +17,17 @@ class SearchDetailController: UIViewController {
     @IBOutlet weak var venueAddressLabel: UILabel!
     @IBOutlet weak var venueTelephoneLabel: UILabel!
     @IBOutlet weak var venueWebsiteLabel: UILabel!
+    @IBOutlet weak var venuePhotoCollectionView: UICollectionView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // MARK: - Properties
-    var viewModel: SearchVenueViewModel?
+    var venue: Venue?
+    var client: FoursquareAPIClient?
+    
+    lazy var collectionViewDataSource: SearchDetailCollectionViewDataSource = {
+       return SearchDetailCollectionViewDataSource(venue: self.venue!, client: self.client!)
+    }()
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -28,9 +36,18 @@ class SearchDetailController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        if let viewModel = viewModel {
-            configure(withViewModel: viewModel)
+        venuePhotoCollectionView.dataSource = collectionViewDataSource
+        if let venue = venue, let client = client {
+            activityIndicator.startAnimating()
+            configure(withViewModel: SearchVenueViewModel(withVenue: venue))
+            updateVenueDetails(forVenue: venue, client: client)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
     func configure(withViewModel viewModel: SearchVenueViewModel) {
@@ -38,5 +55,52 @@ class SearchDetailController: UIViewController {
         venueAddressLabel.text = viewModel.address
         venueTelephoneLabel.text = viewModel.phoneNumber
         venueWebsiteLabel.text = viewModel.url
+    }
+    
+    func updateVenueDetails(forVenue venue: Venue, client: FoursquareAPIClient) {
+        client.updateVenueDetails(venue) { (result) in
+            switch result {
+            case .success(let venue):
+                self.getBestPhoto(forVenue: venue, client: client)
+                self.getVenuePhotos(forVenue: venue, client: client)
+                return
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getBestPhoto(forVenue venue: Venue, client: FoursquareAPIClient) {
+        client.bestPhoto(venue) { (result) in
+            switch result {
+            case .success(let image):
+                UIView.transition(with: self.headerPhotoImageVIew, duration: 0.2, options: .transitionCrossDissolve, animations: { self.headerPhotoImageVIew.image = image}, completion: nil)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getVenuePhotos(forVenue venue: Venue, client: FoursquareAPIClient) {
+        client.getPhotos(forVenue: venue) { (result) in
+            switch result {
+            case .success(let photos):
+                self.collectionViewDataSource.update(withPhotos: photos)
+                self.activityIndicator.stopAnimating()
+                self.venuePhotoCollectionView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension SearchDetailController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
