d4f29aeb2de5d9d489b720eb06278ea5882d2edc
RhLopez <ramiro.h.lopez@outlook.com>
2017-10-30 01:19:03 -0700

    Add VenueServicing

---
+++ AteThere/Services/VenueService.swift
@@ -0,0 +1,13 @@
+//
+//  VenueService.swift
+//  AteThere
+//
+//  Created by Ramiro H Lopez on 10/29/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import Foundation
+
+protocol VenueServicing {
+    func getVenues() -> [Venue]
+}
