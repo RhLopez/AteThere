f61e109efaeeda859df75aaf01d5affdb6ea9fd2
RhLopez <ramiro.h.lopez@outlook.com>
2017-10-31 00:36:38 -0700

    Add HomeTableViewDataSourceTests

---
+++ AteThereTests/Favorites/DataSource/HomeTableViewDataSourceTests.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTableViewDataSourceTests.swift
+//  AteThereTests
+//
+//  Created by Ramiro H Lopez on 10/30/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import XCTest
+@testable import AteThere
+import RealmSwift
+
+class HomeTableViewDataSourceTests: XCTestCase {
+    
+    func test_DataSourceVenuesCount_EqualRealmVenuesCount() {
+        let service = VenueServiceMock(withVenues: [venue("1", name: "First"), venue("2", name: "Second"), venue("3", name: "Third")])
+        let venues = service.getVenues()
+        let sut = HomeTableViewDataSource(withVenues: venues)
+        XCTAssertEqual(sut.tableView(UITableView(), numberOfRowsInSection: 0), venues.count)
+    }
+}
+
+private func venue(_ id: String, name: String) -> Venue {
+    let venue = Venue()
+    venue.id = id
+    venue.name = name
+    
+    return venue
+}
