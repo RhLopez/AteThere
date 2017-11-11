65e848c6ea48735b92db380f102855230ac593b2
RhLopez <ramiro.h.lopez@outlook.com>
2017-11-11 00:39:03 -0800

    Add MealViewController

---
+++ AteThere/Favorites/Contoller/MealViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MealViewController.swift
+//  AteThere
+//
+//  Created by Ramiro H Lopez on 11/9/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MealViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var venue: Venue?
+    var venueServicing: VenueServicing?
+    var dataSource: MealControllerDataSource?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let venue = venue else { return }
+        title = venue.name
+        if let meals = venueServicing?.getMeals(forVenue: venue) {
+            dataSource = MealControllerDataSource(withMeals: meals)
+            tableView.dataSource = dataSource
+        }
+    }
+}
