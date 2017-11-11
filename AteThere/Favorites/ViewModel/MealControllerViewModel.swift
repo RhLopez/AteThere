749390a99f17f1e28efecc0d39e48d7976cf20d5
RhLopez <ramiro.h.lopez@outlook.com>
2017-11-11 00:32:24 -0800

    Add MealControllerViewModel

---
+++ AteThere/Favorites/ViewModel/MealControllerViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  MealControllerViewModel.swift
+//  AteThere
+//
+//  Created by Ramiro H Lopez on 11/10/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import Foundation
+
+struct MealControllerViewModel {
+    let meal: Meal
+    
+    init(withMeal meal: Meal) {
+        self.meal = meal
+    }
+    
+    var name: String {
+        return meal.name
+    }
+    
+    var date: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM d, yyyy"
+        return formatter.string(from: meal.date)
+    }
+    
+    var photoPath: String {
+        return meal.photoPath
+    }
+}
