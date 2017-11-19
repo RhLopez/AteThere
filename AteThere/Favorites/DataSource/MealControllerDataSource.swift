04eb645f3c2fdaea705ae55d2a45b25ef4b2b5fa
RhLopez <ramiro.h.lopez@outlook.com>
2017-11-19 15:14:49 -0800

    Add UICollectionViewDataSource

--- AteThere/Favorites/DataSource/MealControllerDataSource.swift
+++ AteThere/Favorites/DataSource/MealControllerDataSource.swift
@@ -17,24 +17,46 @@ class MealControllerDataSource: NSObject {
     }
 }
 
-// MARK: - UITableViewDataSource
-extension MealControllerDataSource: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+//// MARK: - UITableViewDataSource
+//extension MealControllerDataSource: UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return meals.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: MealTableViewCell.identifier, for: indexPath) as? MealTableViewCell {
+//            let meal = meals[indexPath.row]
+//            cell.configure(withViewModel: MealControllerViewModel(withMeal: meal))
+//
+//            return cell
+//        } else {
+//            return UITableViewCell()
+//        }
+//    }
+//}
+
+// MARK: - UICollectionViewDataSource
+extension MealControllerDataSource: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return meals.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: MealTableViewCell.identifier, for: indexPath) as? MealTableViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MealCollectionViewCell.identifier, for: indexPath) as? MealCollectionViewCell {
             let meal = meals[indexPath.row]
             cell.configure(withViewModel: MealControllerViewModel(withMeal: meal))
             
             return cell
         } else {
-            return UITableViewCell()
+            return UICollectionViewCell()
         }
     }
 }
