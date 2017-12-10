ddd26948052fd1f240b2033aa16a240509917a08
RhLopez <ramiro.h.lopez@outlook.com>
2017-12-10 02:33:42 -0800

    Add UIView extension

---
+++ AteThere/Extension/UIView.swift
@@ -0,0 +1,31 @@
+//
+//  UIView.swift
+//  AteThere
+//
+//  Created by Ramiro H Lopez on 12/9/17.
+//  Copyright © 2017 Ramiro H Lopez. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func snapshot(of rect: CGRect? = nil) -> UIImage? {
+        // snapshot entire view
+        
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
+        drawHierarchy(in: bounds, afterScreenUpdates: true)
+        let wholeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // if no `rect` provided, return image of whole view
+        
+        guard let image = wholeImage, let rect = rect else { return wholeImage }
+        
+        // otherwise, grab specified `rect` of image
+        
+        let scale = image.scale
+        let scaledRect = CGRect(x: rect.origin.x * scale, y: rect.origin.y * scale, width: rect.size.width * scale, height: rect.size.height * scale)
+        guard let cgImage = image.cgImage?.cropping(to: scaledRect) else { return nil }
+        return UIImage(cgImage: cgImage, scale: scale, orientation: .up)
+    }
+}
