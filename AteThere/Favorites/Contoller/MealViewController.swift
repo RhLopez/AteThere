f811d1ac3126aab8c64b2a0d3dc4005997bb4768
RhLopez <ramiro.h.lopez@outlook.com>
2017-12-10 02:35:41 -0800

    Add collectionView

--- AteThere/Favorites/Contoller/MealViewController.swift
+++ AteThere/Favorites/Contoller/MealViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 
 class MealViewController: UICollectionViewController {
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     // MARK: - Properties
     var venue: Venue?
     var venueServicing: VenueServicing?
     var dataSource: MealControllerDataSource?
+    var animator = ExpandAnimator()
+    var selectedIndexPath: IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +29,37 @@ class MealViewController: UICollectionViewController {
             dataSource = MealControllerDataSource(withMeals: meals)
             collectionView?.dataSource = dataSource
         }
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension MealViewController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let meal = dataSource?.meal(at: indexPath),
+            let cell = collectionView.cellForItem(at: indexPath) as? MealCollectionViewCell {
+            
+            self.selectedIndexPath = indexPath
+            
+            animator.selectedCell = cell
+            animator.selectedCellFrame = cell.superview!.convert(cell.frame, to: nil)
+            let mealDetailVC = storyboard?.instantiateViewController(withIdentifier: "MealDetailController") as! MealDetailController
+            mealDetailVC.meal = meal
+            mealDetailVC.mealImage = cell.mealImageView.image
+            mealDetailVC.transitioningDelegate = self
+            present(mealDetailVC, animated: true, completion: nil)
+        }
+    }
+}
+
+// MARK: - UIViewControllerTransitionDelegate
+extension MealViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        animator.presenting = true
+        return animator
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        animator.presenting = false
+        return animator
     }
 }
