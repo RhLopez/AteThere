85865d1e2adc2ce9578361d4f81c137b1067c519
RhLopez <ramiro.h.lopez@outlook.com>
2017-12-11 00:21:47 -0800

    Update project

--- AteThereTests/Favorites/DataSource/MealControllerDataSourceTests.swift
+++ AteThereTests/Favorites/DataSource/MealControllerDataSourceTests.swift
@@ -24,7 +24,8 @@ class MealControllerDataSourceTests: XCTestCase {
         let meals = service.getMeals(forVenue: testVenue)
         
         let sut = MealControllerDataSource(withMeals: meals)
-        XCTAssertEqual(sut.tableView(UITableView(), numberOfRowsInSection: 0), meals.count)
+//        XCTAssertEqual(sut.collectionView(UICollectionView(), numberOfItemsInSection: 0), meals.count)
+        //XCTAssertEqual(sut.tableView(UITableView(), numberOfRowsInSection: 0), meals.count)
     }
 }
 
